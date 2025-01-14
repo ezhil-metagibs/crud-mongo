79032e342cd7bf06de00c567ed1cc418938cb01f
ezhil-dev <ezhilarasan@raptee.com>
2025-01-14 13:45:58 +0530

    connected with mongodb with env file

--- README.md
+++ README.md
@@ -8,3 +8,8 @@
 4. `git add .`
 5. `git commit -m 'your cmd'`
 6. `git push origin 'branch name'`
+
+username: toezhildev
+pass: ALFsfnWpEB3rIufa
+
+mongodb+srv://toezhildev:<db_password>@cluster0.qi5ez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import dataRouter from './routes/routes.js'
+import connectDB from './libs/db.js'
+import dotenv from 'dotenv'
 
 const app = express()
 const port = 3000
+
+dotenv.config()
+
+connectDB()
 
 app.get('/', (req, res)=> {
     res.json({message: 'hello world from router'})
