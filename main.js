c5796e2e9187c96bfade5c0fe8611ec3d9972758
ezhil-dev <ezhilarasan@raptee.com>
2025-01-14 12:03:06 +0530

    route structure

--- main.js
+++ main.js
@@ -3,8 +3,29 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res)=> {
     res.json({message: 'hello world'})
+})
+
+// R - For get all data
+
+app('/api', (req, res) => {
+    
+})
+
+// C - For create a Data
+app.post('/api', () => {
+
+})
+
+// U - for update data
+app.put('/api/:id', () => {
+
+})
+
+// D - for delete data
+app.delete('/api/:id', () => {
+
 })
 
 app.listen(port, ()=>{
